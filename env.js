d3dd75bb556c72a844e0e4f1896d726162dcff9e
kyubongc <kchoi@estech-design.com>
2022-07-11 14:33:25 +0200

    fix(env): mobile test applicated

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 async function GetEnv(callback) {
 	let result = new UAParser().getResult();
 
-	let data = {
+	var data = {
 		es_version: isES6() ? "ES6" : "ES5",
 		monitor: {
 			capacity: {
@@ -21,18 +21,60 @@ async function GetEnv(callback) {
 		memory: { ...GetMemPerformance() },
 		camera: await GetMedia({ video: true }),
 		language : navigator.language.slice(0,2),
-		_3dEnabled: false,
-		_arEnabled: false,
+		_3dEnabled: undefined,
+		_arEnabled: undefined,
 		...result,
 	};
 
-	data._3dEnabled =
-		data.gpu.gl_renderer.includes("WebGL") && isES6() ? true : false;
-	data._arEnabled =
-		data._3dEnabled && data.device.mobile && data.camera ? true : false;
-
+	data._3dEnabled = data.gpu.gl_renderer.includes("WebGL") && isES6() ? true : false;
+	data._arEnabled = GetArCapacity(data._3dEnabled, data.device.model, data.camera)
 	if (callback) callback(data);
 	return data;
+}
+
+//marche pas : /SM-S90*_/ *is number _is alpha_majuscule/number
+//s22	SM-S901B, SM-S901B/DS, SM-S901U, SM-S901U1, SM-S901W, SM-S901N, SM-S9010, SM-S901E, SM-S901E/DS
+// ultra SM-S908B, SM-S908B/DS, SM-S908U, SM-S908U1, SM-S908W, SM-S908N, SM-S9080, SM-S908E, SM-S908E/DS
+// s22+ SM-S906B, SM-S906B/DS, SM-S906U, SM-S906U1, SM-S906W, SM-S906N, SM-S9060, SM-S906E, SM-S906E/DS
+
+//marche : ... /SM-G99/....
+//s21fe SM-G990B, SM-G990B/DS, SM-G990U, SM-G990U1, SM-G990W, SM-G990E
+//s21 SM-G991B, SM-G991B/DS, SM-G991U, SM-G991U1, SM-G991W, SM-G991N, SM-G9910, SM-G991Q
+//s21 ultra SM-G998B, SM-G998B/DS, SM-G998U, SM-G998U1, SM-G998W, SM-G998N, SM-G9980
+//s21 	SM-G998B, SM-G998B/DS, SM-G998U, SM-G998U1, SM-G998W, SM-G998N, SM-G9980
+//s21 + 5g SM-G996B, SM-G996B/DS, SM-G996U, SM-G996U1, SM-G996W, SM-G996N, SM-G9960
+//s21 no 5g 	SM-G990F, SM-G990F/DS
+
+//regex test code
+// const s22s = [
+// 	"SM-S901B", "SM-S901B/DS", "SM-S901U", "SM-S901U1", "SM-S901W", "SM-S901N",
+// 	"SM-S9010", "SM-S901E", "SM-S901E/DS",
+// 	"SM-S908B", "SM-S908B/DS", "SM-S908U", "SM-S908U1",
+// 	"SM-S908W", "SM-S908N", "SM-S9080", "SM-S908E", "SM-S908/DS",
+// 	"SM-S906B", "SM-S906B/DS", "SM-S906U", "SM-S906U1", "SM-S906W", 
+// 	"SM-S906N", "SM-S9060", "SM-S906E", "SM-S906E/DS"
+// ]
+// const s21s = [
+// 	"SM-G990B", "SM-G990B/DS", "SM-G990U" , "SM-G990U1" , "SM-G990W", "SM-G990E"
+// ]
+// //test
+// s22s.forEach(el => {
+// 	console.log("s22s", /^SM-S90[0-9][A-Z, 0-9, \/]/.test(el), el) 
+// })
+// s21s.forEach(el => {
+// 	console.log("s21s", /^SM-S90[0-9][A-Z, 0-9, \/]/.test(el), el) 
+// })
+
+/**
+ * 
+ * @param {boolean} _3dEnabled 
+ * @param {string | undefined} mobile 
+ * @param {boolean} camera 
+ * @return {boolean} 
+ */
+function GetArCapacity(_3dEnabled, mobile, camera ) {
+	if (_3dEnabled && camera && !/^SM-S90[0-9][A-Z, 0-9, \/]/.test(mobile)) return true;
+	return false;
 }
 
 
@@ -73,8 +115,8 @@ function GetMemPerformance() {
 	}
 	let data = {
 		limit_heap_byte : _data.jsHeapSizeLimit,
-		total_heap : `${_data.totalJSHeapSize / _data.jsHeapSizeLimit * 100}%`,
-		used_heap : `${_data.usedJSHeapSize / _data.jsHeapSizeLimit * 100}%`,
+		total_heap_percent : _data.totalJSHeapSize / _data.jsHeapSizeLimit * 100,
+		used_heap_percent : _data.usedJSHeapSize / _data.jsHeapSizeLimit * 100,
 	}
 	return data;
 }
